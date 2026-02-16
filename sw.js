1568f0b1ee01f92bd6f779095b037de9adc7e3da
yeasinpavel <yeasin511436@gmail.com>
2026-02-17 00:00:09 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
-const CACHE_NAME = 'ht-vault-v2';
+const CACHE_NAME = 'ht-vault-v2.6.2';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
   'https://cdn.tailwindcss.com'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
